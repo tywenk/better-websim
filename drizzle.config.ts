4991e7f9067a4e70abb2bf77061a8c710bd60558
Tywen Kelly <tywen.kelly@gmail.com>
2025-04-10 12:59:13 -0400

    fix: point to correct db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   dialect: "sqlite",
   driver: "d1-http",
   dbCredentials: {
-    databaseId: "a5c1bdc8-33c2-45eb-af7e-7efb0636df2c",
+    databaseId: "2e4d0be3-cb47-468e-a3af-fdcb98ed282e",
     accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
     token: process.env.CLOUDFLARE_TOKEN!,
   },
